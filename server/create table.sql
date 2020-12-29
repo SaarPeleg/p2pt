0b5f45cc0400174650b49a4743f8dfe2c8fb5975
SaarPeleg <Saarp6@gmail.com>
2020-12-29 19:50:48 +0200

    fix for database scripts

--- server/create table.sql
+++ server/create table.sql
@@ -19,11 +19,10 @@ CREATE TABLE IF NOT EXISTS Plots (
 );
 
 UPDATE Plots SET product='Avocado' where plotId='0EFF89E7F716F10FB533';*/
+drop table IF EXISTS PlotTempratures;
+
 CREATE TABLE IF NOT EXISTS PlotTempratures (
-	newregIg INTEGER PRIMARY KEY AUTOINCREMENT,
-	userId INTEGER,
-	plotName Text,
-	plotId Text,
+	plotId Text PRIMARY KEY,
 	janTDay Real,
 	febTDay Real,
 	marTDay Real,
@@ -54,14 +53,44 @@ CREATE TABLE IF NOT EXISTS PlotTempratures (
 delete from PlotTempratures;
 
 
-INSERT INTO `PlotTempratures` (userId, plotId,plotName,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
-VALUES (0,"0C0EB1BE0C16F10B47FD","ג'ונגל מזרח",18.5,19.5,23,27,34.5,36.5,39.5,39.5,37,31,26,20.5,7.5,7.5,9,12,17,20,24,24,21,18,13,9.5);
+INSERT INTO `PlotTempratures` (plotId,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
+VALUES ("0C0EB1BE0C16F10B47FD",18.5,19.5,23,27,34.5,36.5,39.5,39.5,37,31,26,20.5,7.5,7.5,9,12,17,20,24,24,21,18,13,9.5);
 
-INSERT INTO `PlotTempratures` (userId, plotId,plotName,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
-VALUES (0,"0EFF89E7F716F10FB533","אנדרטה מערב",18.5,19.5,23,27,33,35,38,38,35.5,31,26,20.5,8.5,8.5,10,13,17,20,24,24,21.5,18,13.8,10.5);
+INSERT INTO `PlotTempratures` (plotId,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
+VALUES ("0EFF89E7F716F10FB533",18.5,19.5,23,27,33,35,38,38,35.5,31,26,20.5,8.5,8.5,10,13,17,20,24,24,21.5,18,13.8,10.5);
 
-INSERT INTO `PlotTempratures` (userId, plotId,plotName,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
-VALUES (0,"08BBA0314716F1122C26","דושן א'",17.5,19,23,27.5,33.5,37.5,39.5,39.5,36,32,25.5,19.5,7.5,8,10,13.5,17.5,20.5,23.5,24.5,22,19,13.5,9.5);
+INSERT INTO `PlotTempratures` (plotId,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
+VALUES ("08BBA0314716F1122C26",17.5,19,23,27.5,33.5,37.5,39.5,39.5,36,32,25.5,19.5,7.5,8,10,13.5,17.5,20.5,23.5,24.5,22,19,13.5,9.5);
 
-INSERT INTO `PlotTempratures` (userId, plotId,plotName,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
-VALUES (0,"0B43D4914116F111112C","המעיין",17.5,18.5,22,26,31,33,36,36,34,29,25,19.5,7.5,7.5,9,12,15,18,22,22,20,16,13,9.5);
+INSERT INTO `PlotTempratures` (plotId,janTDay,febTDay,marTDay,aprTDay,mayTDay,junTDay,julTDay,augTDay,sepTDay,octTDay,novTDay,decTDay,janTNight,febTNight,marTNight,aprTNight,mayTNight,junTNight,julTNight,augTNight,sepTNight,octTNight,novTNight,decTNight)
+VALUES ("0B43D4914116F111112C",17.5,18.5,22,26,31,33,36,36,34,29,25,19.5,7.5,7.5,9,12,15,18,22,22,20,16,13,9.5);
+
+CREATE TABLE IF NOT EXISTS PlotHumidity (
+	plotId Text PRIMARY KEY,
+	jan Real,
+	feb Real,
+	mar Real,
+	apr Real,
+	may Real,
+	jun Real,
+	jul Real,
+	aug Real,
+	sep Real,
+	oct Real,
+	nov Real,
+	dec Real
+);
+
+delete from PlotHumidity;
+
+INSERT INTO `PlotHumidity` (plotId,jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
+VALUES ("0C0EB1BE0C16F10B47FD",71,72,68,62,57,59,60,62,61,60,61,70);
+
+INSERT INTO `PlotHumidity` (plotId,jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
+VALUES ("0EFF89E7F716F10FB533",71,72,68,62,57,59,60,62,61,60,61,70);
+
+INSERT INTO `PlotHumidity` (plotId,jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
+VALUES ("08BBA0314716F1122C26",75,73,66,56,51,52,52,54,53,54,57,68);
+
+INSERT INTO `PlotHumidity` (plotId,jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec)
+VALUES ("0B43D4914116F111112C",68,70,65,57,52,55,59,61,58,55,55,64);
